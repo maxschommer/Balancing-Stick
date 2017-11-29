d6b77f7f3847277ac1c1b15334428ff5a3f6305d
maxschommer <maximilian.schommer@students.olin.edu>
2017-11-28 23:51:04 -0500

    We made a simulation or something

---
+++ MATLABCODE/stickModelODE.m
@@ -0,0 +1,65 @@
+function stickModelODE
+clear
+m = 1;
+theta0 = .1; %Radians
+rodLength = .3;
+thetadot0 = 0;
+g = 9.81;
+integral=0;
+
+P = 20;
+I = 10;
+D = 6;
+
+dt = .001;
+
+tEnd = 10;
+t=0:dt:tEnd;   % time scale
+
+% options = odeset('Events', @springEvents);
+
+[t1, x1] = ode45(@phase1, t, [theta0 thetadot0]);
+
+hold on;
+%Plot phase 1
+plot(t1,x1(:,1)); %Plot mass
+
+figure
+posx = rodLength*sin(x1(:,1));
+posy = rodLength*cos(x1(:,1));
+
+axisLength =rodLength*1.25;
+
+axis([-axisLength, axisLength, -axisLength, axisLength])
+pbaspect([1,1,1])
+% axis(ax, 'square')
+% comet(posx, posy)
+
+h = animatedline('Marker', 'o');
+b = animatedline;
+
+
+for k = 1:10:length(posx)
+    addpoints(h,posx(k),posy(k));
+    addpoints(b, 0,0);
+    addpoints(b, posx(k),posy(k));
+    drawnow
+    clearpoints(h);
+    clearpoints(b);
+end
+
+
+
+function dValues=phase1(t,M)
+    theta = M(1);
+    thetadot = M(2);
+
+    error =theta;
+    integral = integral*.95+theta*dt;
+    a = g*sin(theta) -( P*error + D*thetadot + I*integral);
+
+    dValues=[thetadot; a];
+end
+
+
+end
